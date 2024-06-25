282c7051148237aa4d2f4f1a846ad4979c7b2e54
PrathameshPawar-SDET <pawarprathamesh62@gmail.com>
2024-06-25 23:34:50 +0530

    Code commit on 25-06-2024 23:34

--- src/main/java/com/google_search/Pages/SearchListing.java
+++ src/main/java/com/google_search/Pages/SearchListing.java
@@ -1,8 +1,18 @@
 package com.google_search.Pages;
 
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class SearchListing {
 
@@ -12,6 +22,32 @@ public class SearchListing {
         this.driver = driver;
         PageFactory.initElements(new AjaxElementLocatorFactory(driver, 10), this);
     }
+
+
+
+    @FindBy(xpath = "//div[@class='yuRUbf']/div/span/a[@jsname='UWckNb']")
+    List<WebElement> searchheading;
+
+    public void SearchResult() {
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+            wait.until(ExpectedConditions.visibilityOfAllElements(searchheading));
+            if (searchheading != null && !searchheading.isEmpty()) {
+                String heading = searchheading.get(1).getText();
+                System.out.println(heading);
+
+            } else {
+                System.out.println("No heading found");
+            }
+        }catch (Exception e)
+        {
+            System.out.println("An error occured during execution");
+            e.printStackTrace();
+        }
+
+    }
+
+
 
 
 
--- src/test/java/com/google_search/Test/Testcase1.java
+++ src/test/java/com/google_search/Test/Testcase1.java
@@ -1,4 +1,5 @@
 package com.google_search.Test;
+import com.google_search.Pages.SearchListing;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
@@ -10,14 +11,20 @@ import org.testng.Assert;
 
 public class Testcase1 extends BaseClass {
     HomePage homepage;
+    SearchListing SL;
 
     @Test(description = "Testing for the testcase1")
-    public void searchGoogle() throws InterruptedException{
+    public void searchGoogle() throws Exception {
         driver.get(Constants.baseUrl);
         homepage = new HomePage(driver);
         homepage.searchData("Automation Testing");
         Thread.sleep(2000);
-        Assert.assertEquals(driver.getTitle(), "Automation Testing - Google Search");    
+        Assert.assertEquals(driver.getTitle(), "Automation Testing - Google Search");
+        SL = new SearchListing(driver);
+        SL.SearchResult();
+        Thread.sleep(2000);
+
+
     }
 
 
